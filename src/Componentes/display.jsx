2892db840770be50a5e9bb8ef93096cbb601f790
Juliasouzaalves <87155816+Juliasouzaalves@users.noreply.github.com>
2021-09-01 16:37:55 -0300

    Commit calculadora

---
+++ src/Componentes/display.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import '../Main/CSS/display.css'
+
+export default (props) => <div ClassName="display">{props.value}</div>;
---
+++ src/Main/IndexCalculator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './CSS/index.css';
+import Calculator from './main/Calculator';
+import  reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Calculator />
+    </React.StrictMode>
+    , document.getElementById('root'));
+
+reportWebVitals();
+
+
+
+
